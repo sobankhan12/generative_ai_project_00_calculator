4885a9e61374ab2f8fe8729d077439e4c04bae81
Javed <muhammad.soban@digitify>
2024-03-23 17:35:01 +0500

    add

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    { message: "Enter first number", name: "firstnumber", type: "number" },
+    { message: "Enter second number", name: "secondnumber", type: "number" },
+    { message: "Select operation", name: "operation", type: "list",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"] }
+]);
+console.log(answer);
+let firstnumber = answer.firstnumber;
+let secondnumber = answer.secondnumber;
+let operation = answer.operation;
+switch (operation) {
+    case "Addition":
+        console.log(firstnumber + secondnumber);
+        break;
+    case "Subtraction":
+        console.log(firstnumber - secondnumber);
+        break;
+    case "Multiplication":
+        console.log(firstnumber * secondnumber);
+        break;
+    case "Division":
+        console.log(firstnumber / secondnumber);
+        break;
+}
